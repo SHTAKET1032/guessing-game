043b0fc7fec96b5f7cb3dd0cf5fbb754bfdbcb04
SHTAKET1032 <SHTAKET1032@yandex.ru>
2023-09-22 11:31:33 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.candidate = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        // Используем бинарный поиск для выбора нового кандидата
+        this.candidate = Math.round((this.max - this.min) / 2 + this.min);
+        return this.candidate;
     }
 
     lower() {
-
+        // Устанавливаем верхнюю границу диапазона в текущий кандидат
+        this.max = this.candidate ;
     }
 
     greater() {
-
+        // Устанавливаем нижнюю границу диапазона в текущий кандидат
+        this.min = this.candidate ;
     }
 }
+
 
 module.exports = GuessingGame;
